9494a7a1d3a235e4ef997255573ab4a67a460ebf
Artem <art.ph.djin@gmail.com>
2022-04-28 18:27:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Semenar4_CSharp_Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
